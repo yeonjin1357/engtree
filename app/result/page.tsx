0fc9dd48c443faf68f4e52646ffcaf931e6a0d29
yeonjin1357 <yeonjin1357@naver.com>
2024-04-08 23:11:11 +0900

    수정

---
+++ app/result/page.tsx
@@ -0,0 +1,11 @@
+import classes from "../styles/result.module.css";
+
+const Result = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Result;
---
+++ app/solving/page.tsx
@@ -0,0 +1,11 @@
+import classes from "../styles/solving.module.css";
+
+const Solving = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Solving;
